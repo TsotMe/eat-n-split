d9413917cc97957863628cb4eb56792b6ad846a7
tsotne <tsotnemeskhuri@gmail.com>
2024-09-19 22:22:40 +0400

    Add unipue key to reset state of "FormSplitBill" component

--- src/App.js
+++ src/App.js
@@ -69,6 +69,7 @@ function App() {
       </div>
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
